47298aeef8d83de36ce1137c2635e0475010e567
jiyaosky <jiyaoskywalker@163.com>
2020-07-29 19:21:07 +0800

    first commit

---
+++ src/main/java/com/jesper/seckill/util/UUIDUtil.java
@@ -0,0 +1,15 @@
+package com.jesper.seckill.util;
+
+import java.util.UUID;
+
+/**
+ * <p>
+ * 唯一id生成类
+ */
+public class UUIDUtil {
+
+    public static String uuid() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+
+}
